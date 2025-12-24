ea818ec0c75c9847c45762a5aa34069a6b6c417d
Linqizhi-hue <3310863697@qq.com>
2025-12-24 17:44:49 +0800

    feat:Test cases for bus

--- cpp-training/tests/ExecutorReverseTest.cpp
+++ cpp-training/tests/ExecutorReverseTest.cpp
@@ -32,60 +32,60 @@ TEST(ExecutorReverseTest, should_return_y_plus_1_given_status_is_back_command_is
     const Pose target{0, 1, 'S'};
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorReverseTest, should_return_facing_S_given_status_is_back_command_is_L_and_facing_is_E)
+TEST(ExecutorReverseTest, should_return_x_minus_1_facing_S_given_status_is_back_command_is_L_and_facing_is_E)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
     executor->Execute("BL");
-    const Pose target{0, 0, 'S'};
+    const Pose target{-1, 0, 'S'};
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorReverseTest, should_return_facing_W_given_status_is_back_command_is_L_and_facing_is_S)
+TEST(ExecutorReverseTest, should_return_y_plus_1_facing_W_given_status_is_back_command_is_L_and_facing_is_S)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'}));
     executor->Execute("BL");
-    const Pose target{0, 0, 'W'};
+    const Pose target{0, 1, 'W'};
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorReverseTest, should_return_facing_N_given_status_is_back_command_is_L_and_facing_is_W)
+TEST(ExecutorReverseTest, should_return_x_plus_1_facing_N_given_status_is_back_command_is_L_and_facing_is_W)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'}));
     executor->Execute("BL");
-    const Pose target{0, 0, 'N'};
+    const Pose target{1, 0, 'N'};
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorReverseTest, should_return_facing_E_given_status_is_back_command_is_L_and_facing_is_N)
+TEST(ExecutorReverseTest, should_return_y_minus_1_facing_E_given_status_is_back_command_is_L_and_facing_is_N)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
     executor->Execute("BL");
-    const Pose target{0, 0, 'E'};
+    const Pose target{0, -1, 'E'};
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorReverseTest, should_return_facing_N_given_status_is_back_command_is_R_and_facing_is_E)
+TEST(ExecutorReverseTest, should_return_x_minus_1_facing_N_given_status_is_back_command_is_R_and_facing_is_E)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
     executor->Execute("BR");
-    const Pose target{0, 0, 'N'};
+    const Pose target{-1, 0, 'N'};
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorReverseTest, should_return_facing_W_given_status_is_back_command_is_R_and_facing_is_N)
+TEST(ExecutorReverseTest, should_return_y_minus_1_facing_W_given_status_is_back_command_is_R_and_facing_is_N)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
     executor->Execute("BR");
-    const Pose target{0, 0, 'W'};
+    const Pose target{0, -1, 'W'};
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorReverseTest, should_return_facing_S_given_status_is_back_command_is_R_and_facing_is_W)
+TEST(ExecutorReverseTest, should_return_x_plus_1_facing_S_given_status_is_back_command_is_R_and_facing_is_W)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'}));
     executor->Execute("BR");
-    const Pose target{0, 0,'S'};
+    const Pose target{1, 0,'S'};
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorReverseTest, sould_return_facing_E_given_status_is_back_command_is_R_and_facing_is_S)
+TEST(ExecutorReverseTest, sould_return_y_plus_1_facing_E_given_status_is_back_command_is_R_and_facing_is_S)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'}));
     executor->Execute("BR");
-    const Pose target{0, 0, 'E'};
+    const Pose target{0, 1, 'E'};
     ASSERT_EQ(target, executor->Query());
 }
 TEST(ExecutorReverseTest, should_return_x_minus_2_given_status_is_back_and_fast_command_is_M_and_facing_is_E)
@@ -116,81 +116,81 @@ TEST(ExecutorReverseTest, should_return_y_plus_2_given_status_is_back_and_fast_c
     const Pose target{0, 2, 'S'};
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorReverseTest, should_return_x_minus_1_and_facing_S_given_status_is_back_and_fast_command_is_L_and_facing_is_E)
+TEST(ExecutorReverseTest, should_return_x_minus_2_and_facing_S_given_status_is_back_and_fast_command_is_L_and_facing_is_E)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
     executor->Execute("BFL");
-    const Pose target{-1, 0, 'S'};
+    const Pose target{-2, 0, 'S'};
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorReverseTest, should_return_y_plus_1_and_facing_W_given_status_is_back_and_fast_command_is_L_and_facing_is_S)
+TEST(ExecutorReverseTest, should_return_y_plus_2_and_facing_W_given_status_is_back_and_fast_command_is_L_and_facing_is_S)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'}));
     executor->Execute("BFL");
-    const Pose target{0, 1, 'W'};
+    const Pose target{0, 2, 'W'};
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorReverseTest,should_return_x_plus_1_and_facing_N_given_status_is_back_and_fast_command_is_L_and_facing_is_W)
+TEST(ExecutorReverseTest,should_return_x_plus_2_and_facing_N_given_status_is_back_and_fast_command_is_L_and_facing_is_W)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'}));
     executor->Execute("BFL");
-    const Pose target{1, 0, 'N'};
+    const Pose target{2, 0, 'N'};
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorReverseTest,should_return_y_minus_1_and_facing_E_given_status_is_back_and_fast_command_is_L_and_facing_is_N)
+TEST(ExecutorReverseTest,should_return_y_minus_2_and_facing_E_given_status_is_back_and_fast_command_is_L_and_facing_is_N)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
     executor->Execute("BFL");
-    const Pose target{0, -1, 'E'};
+    const Pose target{0, -2, 'E'};
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorReverseTest,should_return_x_minus_1_and_facing_N_given_status_is_back_and_fast_command_is_R_and_facing_is_E)
+TEST(ExecutorReverseTest,should_return_x_minus_2_and_facing_N_given_status_is_back_and_fast_command_is_R_and_facing_is_E)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
     executor->Execute("BFR");
-    const Pose target{-1, 0, 'N'};
+    const Pose target{-2, 0, 'N'};
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorReverseTest,should_return_y_minus_1_and_facing_W_given_status_is_back_and_fast_command_is_R_and_facing_is_N)
+TEST(ExecutorReverseTest,should_return_y_minus_2_and_facing_W_given_status_is_back_and_fast_command_is_R_and_facing_is_N)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
     executor->Execute("BFR");
-    const Pose target{0, -1, 'W'};
+    const Pose target{0, -2, 'W'};
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorReverseTest,should_return_x_plus_1_and_facing_S_given_status_is_back_and_fast_command_is_R_and_facing_is_W)
+TEST(ExecutorReverseTest,should_return_x_plus_2_and_facing_S_given_status_is_back_and_fast_command_is_R_and_facing_is_W)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'}));
     executor->Execute("BFR");
-    const Pose target{1, 0, 'S'};
+    const Pose target{2, 0, 'S'};
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorReverseTest,should_return_y_plus_1_and_facing_E_given_status_is_back_and_fast_command_is_R_and_facing_is_S)
+TEST(ExecutorReverseTest,should_return_y_plus_2_and_facing_E_given_status_is_back_and_fast_command_is_R_and_facing_is_S)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'}));
     executor->Execute("BFR");
-    const Pose target{0, 1, 'E'};
+    const Pose target{0, 2, 'E'};
     ASSERT_EQ(target, executor->Query());
 }
 TEST(ExecutorReverseTest,should_return_y_plus_1_command_is_BBM_and_facing_is_N)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor());
     executor->Execute("BBM");
-    const Pose target{0, 1, 'N'};
+    const Pose target{0, 2, 'N'};
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorReverseTest, should_return_facing_W_command_is_BBL_and_facing_is_N)
+TEST(ExecutorReverseTest, should_return_y_plus_1_facing_W_command_is_BBL_and_facing_is_N)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor());
     executor->Execute("BBL");
-    const Pose target{0, 0, 'W'};
+    const Pose target{0, 2, 'W'};
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorReverseTest, should_return_facing_E_command_is_BBR_and_facing_is_N)
+TEST(ExecutorReverseTest, should_return_y_plus_1_facing_E_command_is_BBR_and_facing_is_N)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor());
     executor->Execute("BBR");
-    const Pose target{0, 0, 'E'};
+    const Pose target{0, 2, 'E'};
     ASSERT_EQ(target, executor->Query());
 }
 }
--- cpp-training/tests/ExecutorTest.cpp
+++ cpp-training/tests/ExecutorTest.cpp
@@ -8,14 +8,14 @@ namespace adas
 {
 TEST(ExecutorTest, should_return_init_pose_when_without_command)
 {
-    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
-    const Pose target({0, 0, 'E'});
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({1, 0, 'E'}));
+    const Pose target({1, 0, 'E'});
     ASSERT_EQ(target, executor->Query());
 }
 TEST(ExecutorTest, should_return_default_pose_when_without_init_and_command)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor());
-    const Pose target({0, 0, 'N'});
+    const Pose target({0, 1, 'N'});
     ASSERT_EQ(target, executor->Query());
 }
 TEST(ExecutorTest, should_return_x_plus_1_given_command_is_M_and_facing_is_E)
@@ -46,60 +46,60 @@ TEST(ExecutorTest, should_return_y_minus_1_given_command_is_M_and_facing_is_S)
     const Pose target({0, -1, 'S'});
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorTest, should_return_facing_N_given_command_is_L_and_facing_is_E)
+TEST(ExecutorTest, should_return_x_plus_1_facing_N_given_command_is_L_and_facing_is_E)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
     executor->Execute("L");
-    const Pose target({0, 0, 'N'});
+    const Pose target({1, 0, 'N'});
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorTest, should_return_facing_W_given_command_is_L_and_facing_is_N)
+TEST(ExecutorTest, should_return_y_plus_1_facing_W_given_command_is_L_and_facing_is_N)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
     executor->Execute("L");
-    const Pose target({0, 0, 'W'});
+    const Pose target({0, 1, 'W'});
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorTest, should_return_facing_S_given_command_is_L_and_facing_is_W)
+TEST(ExecutorTest, should_return_x_minus_1_facing_S_given_command_is_L_and_facing_is_W)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'}));
     executor->Execute("L");
-    const Pose target({0, 0, 'S'});
+    const Pose target({-1, 0, 'S'});
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorTest, should_return_facing_E_given_command_is_L_and_facing_is_S)
+TEST(ExecutorTest, should_return_y_minus_1_facing_E_given_command_is_L_and_facing_is_S)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'}));
     executor->Execute("L");
-    const Pose target({0, 0, 'E'});
+    const Pose target({0, -1, 'E'});
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorTest, should_return_facing_S_given_command_is_R_and_facing_is_E)
+TEST(ExecutorTest, should_return_x_plus_1_facing_S_given_command_is_R_and_facing_is_E)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
     executor->Execute("R");
-    const Pose target({0, 0, 'S'});
+    const Pose target({1, 0, 'S'});
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorTest, should_return_facing_W_given_command_is_R_and_facing_is_S)
+TEST(ExecutorTest, should_return_y_minus_1_facing_W_given_command_is_R_and_facing_is_S)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'}));
     executor->Execute("R");
-    const Pose target({0, 0, 'W'});
+    const Pose target({0, -1, 'W'});
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorTest, should_return_facing_N_given_command_is_R_and_facing_is_W)
+TEST(ExecutorTest, should_return_x_minus_1_facing_N_given_command_is_R_and_facing_is_W)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'}));
     executor->Execute("R");
-    const Pose target({0, 0, 'N'});
+    const Pose target({-1, 0, 'N'});
     ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorTest, should_return_facing_E_given_command_is_R_and_facing_is_N)
+TEST(ExecutorTest, should_return_y_plus_1_facing_E_given_command_is_R_and_facing_is_N)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
     executor->Execute("R");
-    const Pose target({0, 0, 'E'});
+    const Pose target({0, 1, 'E'});
     ASSERT_EQ(target, executor->Query());
 }
 
